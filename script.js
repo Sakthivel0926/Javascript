c8b04cb8ded91b8f9b3f61c5fb495809cb031b80
Sakthivel0926 <sakthivel.c2022cse@sece.ac.in>
2026-04-28 13:27:39 +0530

    Javascript functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function runCode() {
+
+    var name = "Sakthi";
+
+    const a = 12;
+    const b = 4;
+
+    let sum = a + b;
+    let difference = a - b;
+    let product = a * b;
+    let division = a / b;
+
+    product = product + 15;
+
+    console.log(name);
+    console.log(sum);
+    console.log(difference);
+    console.log(product);
+    console.log(division);
+}
